80a366d41119f2ad2160c8caed24d6c0350dc100
MarharytaKohut <mrhrtkohut@gmail.com>
2023-06-09 22:59:58 +0300

    commit

---
+++ Src/GuessNumber.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class GuessNumber {
+public static void main(String[] args) {
+    Random random = new Random();
+    int a = random.nextInt(10);
+        System.out.println(a);
+}
+}
+
