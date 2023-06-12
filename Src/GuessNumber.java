13ed3ef8b1409fb79d902c375a6de1ac2fbbd8a2
MarharytaKohut <mrhrtkohut@gmail.com>
2023-06-13 00:10:14 +0300

    commit

--- Src/GuessNumber.java
+++ Src/GuessNumber.java
@@ -4,7 +4,20 @@ public class GuessNumber {
 public static void main(String[] args) {
     Random random = new Random();
     int a = random.nextInt(10);
+    while (a == 5) {
         System.out.println(a);
+        if (a == 3){
+            System.out.println(a);
+        } else if ( a == random.nextInt(10)){
+            System.out.println(a);
+    }
+        break;
+    }
+    System.out.println(a);
 }
-}
+
+    }
+
+
+
 
