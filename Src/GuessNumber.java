23c609ed140702a9fbfcecfd25b6b1e68dc79e12
MarharytaKohut <mrhrtkohut@gmail.com>
2023-06-14 13:32:09 +0300

    commit

--- Src/GuessNumber.java
+++ Src/GuessNumber.java
@@ -1,22 +1,25 @@
 import java.util.Random;
 
 public class GuessNumber {
-public static void main(String[] args) {
-    Random random = new Random();
-    int a = random.nextInt(10);
-    while (a == 5) {
-        System.out.println(a);
-        if (a == 3){
-            System.out.println(a);
-        } else if ( a == random.nextInt(10)){
-            System.out.println(a);
+    public static void main(String[] args) {
+        Random random = new Random();
+        int pcNum = random.nextInt(10);
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            int tryNum = random.nextInt(10);
+            if (tryNum == pcNum) {
+                System.out.println("Ви вгадали число " + pcNum);
+                break;
+            }
+            count++;
+                }
+        if (count >= 3) {
+            System.out.println("Було загадане число " + pcNum);
+        }
     }
-        break;
-    }
-    System.out.println(a);
 }
 
-    }
+
 
 
 
